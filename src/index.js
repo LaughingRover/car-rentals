ee43ce80db256c28f4cdc53ab2edd3a91f8466d3
LaughingRover <odohofreh.joseph.i@gmail.com>
2021-03-13 00:22:43 +0000

    Initial commit for car rentals

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Nav from'./components/scripts/Nav'
+import Banner from './components/scripts/Banner';
+import About from './components/scripts/About';
+import Inventory from './components/scripts/Inventory';
+import Contact from './components/scripts/Contact';
+import Footer from './components/scripts/Footer';
+
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		{/* <App /> */}
+
+		<Nav />
+		<Banner />
+
+		<main>
+
+			<About />
+
+			{/* // <!-- division line --> */}
+      <div id="hr">
+				<hr class="container" />
+			</div>
+
+			<Inventory />
+			<Contact />
+			<Footer />
+
+
+		</main>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
